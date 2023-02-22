207323f2ec5e156e646783b78d6aea6b48f92d70
Stephanie Durocher <sdurocher722@gmail.com>
2023-02-21 20:53:12 -0500

    Original Creation

---
+++ script.js
@@ -0,0 +1,35 @@
+//'Who hates eggplant?' "Steven"
+//'Whose favorite flower is a sunflower?' (Rachel)
+//'Whose favorite season is fall?' (Michelle)
+//'Who hates eating Kraft mac and cheese and can only eat mac and cheese they make from scratch?' (Shae)
+//'Who studied infections diseases before switching over to coding?' (Shasanka)
+//'Who was stung by a bee on their tongue?' (Stephanie)
+//'Whose favorite flower is an orchid?' (Kyle)
+//'Who can speak American Sign Language (ASL)?' (Shae)
+//'Who has a phd?' (Urvashi)
+//'Who had broken many bones in their body throughout their life?' Shasanka
+//'Who had lived in the UAE?' (Stephanie and Kyle)
+//'Who is the best Squad Leader?' Erhan
+
+const questions = [
+  {
+    question: "Who hates eggplant?",
+    optionA: "Steven",
+    optionB: "Stephanie",
+    optionC: "Kyle",
+    optionD: "Erhan",
+    correctOption: "optionA",
+  },
+  {
+    question: "Who has their PhD?",
+    optionA: "Urvashi",
+    optionB: "Steven",
+    optionC: "Stephanie",
+    optionD: "Erhan",
+    correctOption: "optionA",
+  },
+];
+
+let shuffledQuestions = [];
+
+function handleQuestions() {}
